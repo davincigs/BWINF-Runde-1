bc4f4ef4b1055fc4348440f75427dc372ec6082f
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2020-11-23 20:27:59 +0100

    add example read function

--- a3-Tobis-Turnier/src/main.rs
+++ a3-Tobis-Turnier/src/main.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::prelude::*;
+use rand::Rng;
+
+const EXAMPLE_PATH: &str = "beispieldaten/spielstaerken1.txt";
+
+#[derive(Debug)]
+struct Example {
+    num_of_competitors: usize,
+    strengths: Vec<usize>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let example = read_example(EXAMPLE_PATH);
+    println!("{:?}", example);
+}
+
+fn read_example(path: &str) -> Example {
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let lines: Vec<&str> = contents.lines().collect();
+
+    let num_of_competitors = lines[0].parse::<usize>().unwrap();
+    let mut strengths = Vec::new();
+
+    for i in 1..num_of_competitors + 1 {
+        let strength = lines[i].parse::<usize>().unwrap();
+        strengths.push(strength);
+    }
+
+    Example {
+        num_of_competitors,
+        strengths,
+    }
 }
