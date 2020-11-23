7fd6124a4309057f3e95f8161d1082d3a2a82275
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2020-11-23 21:43:54 +0100

    add league system

--- a3-Tobis-Turnier/src/main.rs
+++ a3-Tobis-Turnier/src/main.rs
@@ -1,46 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::fs::File;
 use std::io::prelude::*;
-use rand::Rng;
 
 const EXAMPLE_PATH: &str = "beispieldaten/spielstaerken1.txt";
+const NUM_OF_RUNS: usize = 1000;
 
 #[derive(Debug)]
 struct Example {
-    num_of_competitors: usize,
+    num_of_players: usize,
     strengths: Vec<usize>,
 }
 
-fn main() {
-    let mut example = read_example(EXAMPLE_PATH);
-    println!("{:?}", example);
-
-    for i in 0..=example.strengths.len() {
-        for j in i..example.strengths.len() {
-            if competition(example.strengths[i], example.strengths[j]) {
-                example.strengths[i] = example.strengths[i] + 1;
-            } else {
-                example.strengths[j] = example.strengths[j] + 1;
-            }
-        }
-    }
-
-    println!("{:?}", example);
+fn best_player_index(wins: &Vec<usize>) -> usize {
+    wins.iter()
+        .enumerate()
+        .max_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap_or(Ordering::Equal))
+        .map(|(index, _)| index)
+        .unwrap()
 }
 
-fn competition(ps1: usize, ps2: usize) -> bool {
-    let sum = (ps1 + ps2) as f32;    
-    let mut rng = rand::thread_rng();
-    let mut rand = rng.gen::<f32>() * sum;
+fn main() {
+    let example = read_example(EXAMPLE_PATH);
+    let mut league_winners = vec![0; example.num_of_players];
 
-    while rand == ps1 as f32 {
-        rand = rng.gen::<f32>() * sum
+    for _ in 0..NUM_OF_RUNS {
+        league_winners[league_system(&example.strengths)] += 1;
     }
 
-    if rand < ps1 as f32 {
-        return true;
-    }
-    
-    false
+    println!("{:?}", best_player_index(&league_winners));
 }
 
 fn read_example(path: &str) -> Example {
@@ -49,16 +37,52 @@ fn read_example(path: &str) -> Example {
     file.read_to_string(&mut contents).unwrap();
     let lines: Vec<&str> = contents.lines().collect();
 
-    let num_of_competitors = lines[0].parse::<usize>().unwrap();
+    let num_of_players = lines[0].parse::<usize>().unwrap();
     let mut strengths = Vec::new();
 
-    for i in 1..=num_of_competitors {
+    for i in 1..=num_of_players {
         let strength = lines[i].parse::<usize>().unwrap();
         strengths.push(strength);
     }
 
     Example {
-        num_of_competitors,
+        num_of_players,
         strengths,
     }
+}
+
+fn confrontation(h: usize, g: usize) -> bool {
+    let mut rng = rand::thread_rng();
+
+    if h == g {
+        return rng.gen::<bool>();
+    }
+
+    let sum = h + g;
+    let rand = rng.gen_range(1, sum + 1);
+
+    if rand <= h {
+        return true;
+    }
+    false
+}
+
+fn league_system(strengths: &Vec<usize>) -> usize {
+    let mut wins: Vec<usize> = vec![0; strengths.len()];
+
+    for i in 0..strengths.len() {
+        for j in i + 1..strengths.len() {
+            if confrontation(strengths[i], strengths[j]) {
+                wins[i] += 1;
+            } else {
+                wins[j] += 1;
+            }
+        }
+    }
+
+    return wins.iter().max().unwrap().clone();
+}
+
+fn knockout_system(strengths: Vec<usize>) -> usize {
+    0
 }
