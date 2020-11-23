d61429d34f654c06343cfece0258270a729c09c8
Noél Sigmunczyk <noelsigmunczyk@users.noreply.github.com>
2020-11-23 20:47:15 +0100

    add competition function

--- a3-Tobis-Turnier/src/main.rs
+++ a3-Tobis-Turnier/src/main.rs
@@ -11,8 +11,36 @@ struct Example {
 }
 
 fn main() {
-    let example = read_example(EXAMPLE_PATH);
+    let mut example = read_example(EXAMPLE_PATH);
     println!("{:?}", example);
+
+    for i in 0..=example.strengths.len() {
+        for j in i..example.strengths.len() {
+            if competition(example.strengths[i], example.strengths[j]) {
+                example.strengths[i] = example.strengths[i] + 1;
+            } else {
+                example.strengths[j] = example.strengths[j] + 1;
+            }
+        }
+    }
+
+    println!("{:?}", example);
+}
+
+fn competition(ps1: usize, ps2: usize) -> bool {
+    let sum = (ps1 + ps2) as f32;    
+    let mut rng = rand::thread_rng();
+    let mut rand = rng.gen::<f32>() * sum;
+
+    while rand == ps1 as f32 {
+        rand = rng.gen::<f32>() * sum
+    }
+
+    if rand < ps1 as f32 {
+        return true;
+    }
+    
+    false
 }
 
 fn read_example(path: &str) -> Example {
@@ -24,7 +52,7 @@ fn read_example(path: &str) -> Example {
     let num_of_competitors = lines[0].parse::<usize>().unwrap();
     let mut strengths = Vec::new();
 
-    for i in 1..num_of_competitors + 1 {
+    for i in 1..=num_of_competitors {
         let strength = lines[i].parse::<usize>().unwrap();
         strengths.push(strength);
     }
